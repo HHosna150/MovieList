3ebace6574e21c2dfca7e9cbd46d689e26e3cd32
HHosna150 <53860833+HHosna150@users.noreply.github.com>
2024-06-13 18:17:34 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import movie from "./routes/movie.js";
+import connection from "./db.js";
+
+dotenv.config(); /* Load .env file contents into process.env */
+//require('dotenv').config();
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+/* routes */
+app.use("/movie", movie);
+
+app.get("/user", (req, res) => {
+  connection.query(`SELECT * FROM bookmarks`, (err, result) => {
+    if (err) {
+      console.error(err);
+    }
+    res.send(result);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on http://localhost:${PORT}`);
+});
