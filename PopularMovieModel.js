75cd478e09d0c1f50875a9a0cd83b170a2f358dd
HHosna150 <53860833+HHosna150@users.noreply.github.com>
2024-06-24 02:07:27 -0400

    Update PopularMovieModel.js

--- PopularMovieModel.js
+++ PopularMovieModel.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { MovieCard } from "../dtos/MovieDto.js";
 
-export const getPopularMovie = async (page) => {
-  const data = [];
+export const getPopularMovie = async (pageNumber) => {
+  const movieList = [];
 
   try {
+    // Fetch popular movies from the API
     const response = await axios.get(
       "https://api.themoviedb.org/3/movie/popular",
       {
@@ -14,20 +15,20 @@ export const getPopularMovie = async (page) => {
         },
         params: {
           language: "en-US",
-          page: page,
+          page: pageNumber,
         },
       }
     );
 
-    /* Data Cleaning */
+    /* Clean and structure fetched data */
     const { results, total_pages } = response.data;
 
-    results.map((movie) => {
-      data.push(new MovieCard(movie.title, movie.poster_path, movie.id));
+    results.forEach((movie) => {
+      movieList.push(new MovieCard(movie.title, movie.poster_path, movie.id));
     });
 
-    return { movies: data, total_pages: total_pages };
-  } catch (e) {
-    throw e;
+    return { movies: movieList, total_pages: total_pages };
+  } catch (error) {
+    throw error;
   }
 };
