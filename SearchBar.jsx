46992d6bf0505ef097d8c8ea220d0d0a6a0fe87e
HHosna150 <53860833+HHosna150@users.noreply.github.com>
2024-06-24 01:56:33 -0400

    Update SearchBar.jsx

--- SearchBar.jsx
+++ SearchBar.jsx
@@ -1,27 +1,29 @@
-import React, { useState } from "react";
+import React from "react";
 import { FaSearch } from "react-icons/fa";
 import "./SearchBar.css";
 
 const SearchBar = ({ fetchMoviesBySearch, fetchPopularMovie }) => {
-  const handleChange = (value) => {
-    if (value !== "") {
-      fetchMoviesBySearch(value);
+  // Handle input change
+  const handleChange = (e) => {
+    const { value } = e.target;
+    if (value.trim() !== "") {
+      fetchMoviesBySearch(value.trim()); // Fetch movies by search term
     } else {
-      fetchPopularMovie(1);
+      fetchPopularMovie(1); // Fetch popular movies if search term is empty
     }
   };
 
   return (
-    <>
-      <div className="input-wrapper">
-        <FaSearch id="search-icon" />
+    <div className="input-wrapper">
+      <FaSearch id="search-icon" />
 
-        <input
-          placeholder="Type to search..."
-          onChange={(e) => handleChange(e.target.value)}
-        />
-      </div>
-    </>
+      <input
+        type="text"
+        placeholder="Search for movies..."
+        onChange={handleChange}
+        className="search-input"
+      />
+    </div>
   );
 };
 
