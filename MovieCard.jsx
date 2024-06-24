6da38b77a2bf1ba5ca43994916587f907a513801
HHosna150 <53860833+HHosna150@users.noreply.github.com>
2024-06-24 03:00:13 -0400

    Update MovieCard.jsx

--- MovieCard.jsx
+++ MovieCard.jsx
@@ -1,70 +1,45 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import {
-  Container,
-  Card,
-  Button,
-  Placeholder,
-  Row,
-  Col,
-} from "react-bootstrap";
+import { Container, Card, Button, Row, Col } from "react-bootstrap";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import noimage from "../assets/images/no-image.png";
-import "react-lazy-load-image-component/src/effects/blur.css";
-import "./MovieCard.css";
+import "./MovieCard.css"; // Assuming you have additional styles in this file
 
-const MovieCard = ({
-  title,
-  poster_path,
-  id,
-  isBookmarked,
-  handleBookmarkToggle,
-}) => {
-  const path = `https://image.tmdb.org/t/p/original${poster_path}`;
+const MovieCard = ({ title, poster_path, id, handleBookmarkToggle }) => {
+  const path = poster_path
+    ? `https://image.tmdb.org/t/p/original${poster_path}`;
+
+  const handleBookmarkClick = () => {
+    handleBookmarkToggle(id, title, poster_path);
+  };
 
   return (
-    <>
+    <Card className="movie-card" style={{ marginBottom: "1.5rem", border: "none" }}>
       <Link to={`/movie/${id}`}>
-        <Card
-          style={{ width: "18rem", marginBottom: "1.5rem", border: "none" }}
-        >
-          {poster_path === null ? (
-            <LazyLoadImage
-              src={noimage}
-              placeholderSrc={path}
-              effect="blur"
-              className="movie-image"
-            />
-          ) : (
-            <LazyLoadImage
-              src={path}
-              placeholderSrc={path}
-              effect="blur"
-              className="movie-image"
-            />
-          )}
-        </Card>
+        <LazyLoadImage
+          src={path}
+          alt={title}
+          effect="blur"
+          className="movie-image"
+        />
       </Link>
-
-      <Container>
-        <Row>
-          <Col md={{ span: 8 }}>
-            <p>{title}</p>
+      <Container className="movie-details">
+        <Row className="justify-content-between align-items-center">
+          <Col md={8}>
+            <p className="movie-title">{title}</p>
           </Col>
-          <Col>
+          <Col md={4} className="d-flex justify-content-end">
             <Button
               id="add-btn"
-              onClick={() => handleBookmarkToggle(id, title, poster_path)}
+              variant="dark"
+              onClick={handleBookmarkClick}
             >
-              add
+              Add
             </Button>
           </Col>
         </Row>
       </Container>
-    </>
+    </Card>
   );
 };
 
 export default MovieCard;
-
-
