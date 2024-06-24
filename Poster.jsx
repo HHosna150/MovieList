c6ebc510b47d288617636afa7b386d5333e4b498
HHosna150 <53860833+HHosna150@users.noreply.github.com>
2024-06-24 02:40:22 -0400

    Update Poster.jsx

--- Poster.jsx
+++ Poster.jsx
@@ -4,24 +4,23 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import "./Poster.css";
 
-const Poster = ({ detail, director }) => {
-  const { backdrop_path, poster_path } = detail;
+const Poster = ({ detail }) => {
+  const { poster_path } = detail;
 
-  const poster = `https://image.tmdb.org/t/p/original${poster_path}`;
-  const backdrop = `https://image.tmdb.org/t/p/original${backdrop_path}`;
+  const posterUrl = `https://image.tmdb.org/t/p/original${poster_path}`;
 
   return (
-    <>
-      <Container className="info-page">
-        <LazyLoadImage
-          width={300}
-          src={poster}
-          effect="blur"
-          style={{ borderRadius: "10" }}
-        />
-      </Container>
-    </>
+    <Container className="poster-container">
+      <LazyLoadImage
+        src={posterUrl}
+        effect="blur"
+        width={300}
+        alt="Movie Poster"
+        className="poster-image"
+      />
+    </Container>
   );
 };
 
 export default Poster;
+
